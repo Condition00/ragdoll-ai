3d2d786fc6867e7d5815b2e8e3ec47653c1bb60a
Condition00 <anant.kavuru@gmail.com>
2026-09-15 14:20:58 +0530

    Window

--- ragdoll.cpp
+++ ragdoll.cpp
@@ -1,6 +1,44 @@
+#include<iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using namespace std;
+
 int main() {
+    if (!glfwInit()) {
+        cerr << "Failed to initialize GLFW\n";
+        return 1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Ragdoll", nullptr, nullptr);
+
+    if(!window) {
+        cerr << "Failed to create a window\n";
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        cerr << "Failed to initialize GLAD\n";
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1;
+    }
+
+    cout << "OpenGL: " << glGetString(GL_VERSION) << '\n';
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
